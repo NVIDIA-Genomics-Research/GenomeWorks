ee3efa8cca999e203ca95919631f6eb35261c616
Mike Vella <mvella@nvidia.com>
2019-04-12 11:48:30 +0100

    Fixed index.cpp missing namespace

--- cudamapper/src/index.cpp
+++ cudamapper/src/index.cpp
@@ -4,35 +4,37 @@
 #include "bioparser/bioparser.hpp"
 #include "cudamapper/sequence.hpp"
 
-index::index() {}
+namespace cudamapper {
+    Index::Index() {}
 
-void index::generate_index(char *fasta_filename) {
+    void Index::generate_index(char *fasta_filename) {
 
-    std::string query_filename = std::string(fasta_filename);
+        std::string query_filename = std::string(fasta_filename);
 
-    std::unique_ptr<bioparser::Parser<Sequence>> query_parser = nullptr;
+        std::unique_ptr <bioparser::Parser<Sequence>> query_parser = nullptr;
 
-    auto is_suffix = [](const std::string& src, const std::string& suffix) -> bool {
-        if (src.size() < suffix.size()) {
-            return false;
+        auto is_suffix = [](const std::string &src, const std::string &suffix) -> bool {
+            if (src.size() < suffix.size()) {
+                return false;
+            }
+            return src.compare(src.size() - suffix.size(), suffix.size(), suffix) == 0;
+        };
+
+        if (is_suffix(query_filename, ".fasta") || is_suffix(query_filename, ".fa") ||
+            is_suffix(query_filename, ".fasta.gz") || is_suffix(query_filename, ".fa.gz")) {
+            std::cout << "Getting Query data" << std::endl;
+            query_parser = bioparser::createParser<bioparser::FastaParser, Sequence>(
+                    query_filename);
         }
-        return src.compare(src.size() - suffix.size(), suffix.size(), suffix) == 0;
-    };
 
-    if (is_suffix(query_filename, ".fasta") || is_suffix(query_filename, ".fa") ||
-        is_suffix(query_filename, ".fasta.gz") || is_suffix(query_filename, ".fa.gz")) {
-        std::cout<<"Getting Query data" <<std::endl;
-        query_parser = bioparser::createParser<bioparser::FastaParser, Sequence>(
-                query_filename);
+        //read the query file:
+        std::vector <std::unique_ptr<Sequence>> fasta_objects;
+        query_parser->parse(fasta_objects, -1);
+
+        //We can read the data:
+        for (int i = 0; i < fasta_objects.size(); i++) {
+            std::cout << i << ": " << fasta_objects[i]->name() << std::endl;
+            std::cout << i << ": " << fasta_objects[i]->data() << std::endl;
+        }
     }
-
-    //read the query file:
-    std::vector<std::unique_ptr<Sequence>> fasta_objects;
-    query_parser->parse(fasta_objects, -1);
-
-    //We can read the data:
-    for (int i = 0; i < fasta_objects.size(); i++){
-        std::cout << i << ": " << fasta_objects[i]->name() << std::endl;
-        std::cout << i << ": " << fasta_objects[i]->data() << std::endl;
-        }
 }
