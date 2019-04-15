6d357a0fe1736ea24a0e88ecd086ef9dc2a21c0b
Mike Vella <mvella@nvidia.com>
2019-04-15 18:32:38 +0100

    Removed private members from  abstract base class

--- cudamapper/include/cudamapper/sequence.hpp
+++ cudamapper/include/cudamapper/sequence.hpp
@@ -10,9 +10,5 @@ public:
     virtual const std::string &name() const = 0;
     virtual const std::string &data() const = 0;
     static std::unique_ptr<Sequence> create_sequence();
-
-private:
-    std::string name_;
-    std::string data_;
 };
 }
