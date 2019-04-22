cb47ea139335dd8e65c3b470463dec2bcd25d743
Lars Bishop <lbishop@nvidia.com>
2019-04-22 09:47:47 -0700

    Adding a top-level welcome page to the SDK docs, as well as CMake and doxygen configs to compile it.

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,6 +1,7 @@
 cmake_minimum_required(VERSION 3.10.2)
 
 set_property(GLOBAL PROPERTY doxygen_src_dirs)
+set_property(GLOBAL PROPERTY doxygen_mainpage)
 function(add_doxygen_source_dir)
     get_property(tmp GLOBAL PROPERTY doxygen_src_dirs)
     # avoid leading space in path
@@ -15,11 +16,20 @@ function(add_doxygen_source_dir)
     set_property(GLOBAL PROPERTY doxygen_src_dirs "${tmp}")
 endfunction(add_doxygen_source_dir)
 
+function(set_doxygen_mainpage page)
+    set_property(GLOBAL PROPERTY doxygen_mainpage ${page})
+    add_doxygen_source_dir(${page})
+endfunction(set_doxygen_mainpage)
+
 function(add_docs_target DOXY_PACKAGE_NAME DOXY_PACKAGE_VERSION)
     find_package(Doxygen)
     if(DOXYGEN_FOUND)
         set(DOXYGEN_PROJECT_NAME ${DOXY_PACKAGE_NAME})
         set(DOXYGEN_PROJECT_NUMBER ${DOXY_PACKAGE_VERSION})
+
+        get_property(final_doxygen_mainpage GLOBAL PROPERTY doxygen_mainpage)
+
+        set(DOXYGEN_USE_MDFILE_AS_MAINPAGE ${final_doxygen_mainpage})
 
         get_property(final_doxygen_src_dirs GLOBAL PROPERTY doxygen_src_dirs)
         doxygen_add_docs(docs ALL ${final_doxygen_src_dirs})
