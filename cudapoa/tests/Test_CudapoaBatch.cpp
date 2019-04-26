8384dece4775b358d90b59322802181b62459027
Joyjit Daw <jdaw@nvidia.com>
2019-04-26 10:08:52 -0700

    [tests] sample gtest

---
+++ cudapoa/tests/Test_CudapoaBatch.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "cudapoa/cudapoa_batch.hpp"
+
+namespace genomeworks {
+
+namespace cudapoa {
+
+class TestCudapoaBatch : public ::testing::Test {
+    public:
+        void SetUp()
+        {
+            // Do noting for now, but place for
+            // constructing test objects.
+        }
+};
+
+TEST_F(TestCudapoaBatch, DummyTest)
+{
+    ASSERT_EQ(StatusType::SUCCESS, StatusType::SUCCESS);
+}
+
+}
+
+}
