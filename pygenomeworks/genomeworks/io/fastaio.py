f2668d9843155baa4b6e4daf366e2438b9a09bbb
Mike Vella <mvella@nvidia.com>
2019-05-01 15:01:59 +0100

    Reduced memory footprint and improved mulitprocessing performance

--- pygenomeworks/genomeworks/io/fastaio.py
+++ pygenomeworks/genomeworks/io/fastaio.py
@@ -1,27 +1,34 @@
 """
 Functions for FASTA file I/O
 """
+import gzip
 
 
-def write_fasta(seqs, filepath):
+def write_fasta(seqs, filepath, gzip_compressed=False):
     """Writes a fasta file for sequences.
 
     Args:
       seqs: list of 2-tuples containnig sequnces and their names, e.g [('seq1', 'ACGTC...'), ('seq2', 'TTGGC...'), ...]]
       filepath: path to file for writing out FASTA.
+      gzip_compressed bool: If True then the read component of the sequence has been compressed with gzip
 
     Returns:
       None.
     """
-    fasta_string = ""
-    for s in seqs:
-        fasta_string += ">{}\n".format(s[0])
-
-        lines = [s[1][n*80:(n+1)*80] for n in range((len(s[1])//80)+1)]
-
-        fasta_string += "\n".join(lines)
-        if fasta_string[-1] != "\n":
-            fasta_string += "\n"
 
     with open(filepath, 'w') as f:
-        f.write(fasta_string)
+        for s in seqs:
+            fasta_string = ">{}\n".format(s[0])
+
+            if gzip_compressed:
+                read = str(gzip.decompress(s[1]), "utf-8")
+            else:
+                read = s[1]
+
+            lines = [read[n*80:(n+1)*80] for n in range((len(read)//80)+1)]
+
+            fasta_string += "\n".join(lines)
+            if fasta_string[-1] != "\n":
+                fasta_string += "\n"
+
+            f.write(fasta_string)
