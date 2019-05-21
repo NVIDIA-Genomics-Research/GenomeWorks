4b8b4ff1d8b283b170dda0be2a70718a1a9b0da1
Joyjit Daw <jdaw@nvidia.com>
2019-05-21 12:12:26 -0700

    [gw] adding copyright

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# Copyright (c) 2019, NVIDIA CORPORATION.
+#
+# Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+
 ######################################
 # GenomeWorks CPU build script for CI #
 ######################################
