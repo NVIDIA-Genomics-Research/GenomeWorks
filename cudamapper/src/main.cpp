f983e5a8880cbd3f0835875d3e1b8ad2a3953fa4
Milos Maric <mmaric@nvidia.com>
2019-05-31 06:08:45 -0700

    [cudamapper] Added a data structure that for each sequence has a list of mappings of representations from that sequence to same representations it other sequences

--- cudamapper/src/main.cpp
+++ cudamapper/src/main.cpp
@@ -11,18 +11,33 @@
 #include <string>
 
 #include "cudamapper/index.hpp"
+#include "matcher.hpp"
 #include <logging/logging.hpp>
+
+#include <iostream>
+#include <chrono>
 
 int main(int argc, char *argv[])
 {
     genomeworks::logging::Init();
-
+    auto start_time = std::chrono::high_resolution_clock::now();
     GW_LOG_INFO("Creating index generator");
     // TODO: pass kmer and window size as parameters
-    std::unique_ptr<genomeworks::IndexGenerator> index_generator = genomeworks::IndexGenerator::create_index_generator(std::string(argv[1]), 4, 4);
+    std::unique_ptr<genomeworks::IndexGenerator> index_generator = genomeworks::IndexGenerator::create_index_generator(std::string(argv[1]), 15, 15);
     GW_LOG_INFO("Created index generator");
+    std::cout << "Index generator execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
+
+    start_time = std::chrono::high_resolution_clock::now();
     GW_LOG_INFO("Creating index");
     std::unique_ptr<genomeworks::Index> index = genomeworks::Index::create_index(*index_generator);
     GW_LOG_INFO("Created index");
+    std::cout << "Index execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
+
+    start_time = std::chrono::high_resolution_clock::now();
+    GW_LOG_INFO("Started matcher");
+    genomeworks::Matcher matcher(static_cast<genomeworks::IndexGPU&>(*(index.get())));
+    GW_LOG_INFO("Finished matcher");
+    std::cout << "Matcher execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
+
     return  0;
 }
