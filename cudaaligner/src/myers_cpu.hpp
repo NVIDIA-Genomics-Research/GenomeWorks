aeda5ab5606350061b247cd3de2235f14d67ab63
Andreas Hehn <ahehn@nvidia.com>
2019-06-16 18:11:25 +0200

    [cudaaligner] Myers CPU added blocking

--- cudaaligner/src/myers_cpu.hpp
+++ cudaaligner/src/myers_cpu.hpp
@@ -12,6 +12,7 @@
 
 #include <cassert>
 #include <climits>
+#include <vector>
 #include <utils/signed_integer_utils.hpp>
 
 namespace claragenomics
@@ -22,27 +23,34 @@ namespace cudaaligner
 
 using WordType = uint32_t;
 
-int32_t myers_advance_block(WordType hmask, WordType eq, WordType & pv, WordType & mv)
+int32_t myers_advance_block(WordType hmask, int32_t carry_in, WordType eq, WordType& pv, WordType& mv)
 {
-    assert( (pv & mv) == WordType(0) );
+    assert((pv & mv) == WordType(0));
 
     // Stage 1
     WordType xv = eq | mv;
+    if(carry_in < 0)
+        eq |= WordType(1);
     WordType xh = (((eq & pv) + pv) ^ pv) | eq;
     WordType ph = mv | (~(xh | pv));
     WordType mh = pv & xh;
 
-    int32_t carry = ((ph & hmask) == WordType(0) ? 0 : 1)
-       - ((mh & hmask) == WordType(0) ? 0 : 1);
+    int32_t carry_out = ((ph & hmask) == WordType(0) ? 0 : 1) - ((mh & hmask) == WordType(0) ? 0 : 1);
 
     ph <<= 1;
     mh <<= 1;
+
+    if(carry_in < 0)
+        mh |= WordType(1);
+
+    if(carry_in > 0)
+        ph |= WordType(1);
 
     // Stage 2
     pv = mh | (~(xv | ph));
     mv = ph & xv;
 
-    return carry;
+    return carry_out;
 }
 
 WordType myers_preprocess(char x, std::string const& query, int32_t offset)
@@ -60,45 +68,55 @@ WordType myers_preprocess(char x, std::string const& query, int32_t offset)
 
 int32_t myers_compute_edit_distance(std::string const& target, std::string const& query)
 {
-    std::cout << "q:" << query.size() << std::endl;
-    std::cout << "W:" << sizeof(WordType)*CHAR_BIT << std::endl;
-    assert( query.size() <= sizeof(WordType)*CHAR_BIT );
+    constexpr int32_t word_size = sizeof(WordType) * CHAR_BIT;
+    const int32_t query_size    = get_size(query);
 
-    if(get_size(query) == 0)
+    if (query_size == 0)
         return get_size(target);
 
-    const WordType peq_a = myers_preprocess('A', query, 0);
-    const WordType peq_c = myers_preprocess('C', query, 0);
-    const WordType peq_g = myers_preprocess('G', query, 0);
-    const WordType peq_t = myers_preprocess('T', query, 0);
+    const int32_t n_blocks = (query_size + word_size - 1) / word_size;
 
-    WordType pv = ~ WordType(0);
-    WordType mv =   WordType(0);
-    int32_t score = get_size(query);
-    const WordType hmask = WordType(1) << (get_size(query) - 1);
+    std::vector<WordType> pv(n_blocks, ~WordType(0));
+    std::vector<WordType> mv(n_blocks, 0);
+    std::vector<int32_t> score(n_blocks);
+    for (int32_t i = 0; i < n_blocks; ++i)
+    {
+        score[i] = (std::min)((i + 1) * word_size, query_size);
+    }
+
     for (const char t : target)
     {
-        const WordType eq = [peq_a, peq_c, peq_g, peq_t](char x) -> WordType {
-            assert(x == 'A' || x == 'C' || x == 'G' || x == 'T');
-            switch (x)
-            {
-            case 'A':
-                return peq_a;
-            case 'C':
-                return peq_c;
-            case 'G':
-                return peq_g;
-            case 'T':
-                return peq_t;
-            default:
-                return 0;
-            }
-        }(t);
+        int32_t carry = 0;
+        for (int32_t i = 0; i < n_blocks; ++i)
+        {
+            const WordType peq_a = myers_preprocess('A', query, i * word_size);
+            const WordType peq_c = myers_preprocess('C', query, i * word_size);
+            const WordType peq_g = myers_preprocess('G', query, i * word_size);
+            const WordType peq_t = myers_preprocess('T', query, i * word_size);
+            const WordType hmask = WordType(1) << (i < (n_blocks - 1) ? word_size - 1 : query_size - (n_blocks - 1) * word_size - 1);
 
-        int32_t carry = myers_advance_block(hmask, eq, pv, mv);
-        score += carry;
+            const WordType eq = [peq_a, peq_c, peq_g, peq_t](char x) -> WordType {
+                assert(x == 'A' || x == 'C' || x == 'G' || x == 'T');
+                switch (x)
+                {
+                case 'A':
+                    return peq_a;
+                case 'C':
+                    return peq_c;
+                case 'G':
+                    return peq_g;
+                case 'T':
+                    return peq_t;
+                default:
+                    return 0;
+                }
+            }(t);
+
+            carry = myers_advance_block(hmask, carry, eq, pv[i], mv[i]);
+            score[i] += carry;
+        }
     }
-    return score;
+    return score.back();
 }
 
 } // namespace cudaaligner
--- cudaaligner/tests/Test_MyersAlgorithm.cpp
+++ cudaaligner/tests/Test_MyersAlgorithm.cpp
@@ -30,53 +30,63 @@ std::vector<TestCaseData> create_myers_test_cases()
 
     TestCaseData t;
 
-    t.target = "AAAAAAAAAA";
-    t.query  = "CGTCGTCGTC";
+    t.target        = "AAAAAAAAAA";
+    t.query         = "CGTCGTCGTC";
     t.edit_distance = 10;
     tests.push_back(t);
 
-    t.target = "AATAATAATA";
-    t.query  = "CGTCGTCGTC";
+    t.target        = "AATAATAATA";
+    t.query         = "CGTCGTCGTC";
     t.edit_distance = 7;
     tests.push_back(t);
 
-    t.target = "AATAATAATA";
-    t.query  = "";
+    t.target        = "AATAATAATA";
+    t.query         = "";
     t.edit_distance = 10;
     tests.push_back(t);
 
-    t.target = "";
-    t.query  = "CGTCGTCGTC";
+    t.target        = "";
+    t.query         = "CGTCGTCGTC";
     t.edit_distance = 10;
     tests.push_back(t);
 
-    t.target = "CGTCGTCGTC";
-    t.query  = "CGTCGTCGTC";
+    t.target        = "CGTCGTCGTC";
+    t.query         = "CGTCGTCGTC";
     t.edit_distance = 0;
     tests.push_back(t);
 
-    t.target = "CGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGT";
-    t.query  = "AGTCGTCGTCCGTAATCGTCCGTCGTCGTCGA";
+    t.target        = "CGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGT";
+    t.query         = "AGTCGTCGTCCGTAATCGTCCGTCGTCGTCGA";
     t.edit_distance = 4;
     tests.push_back(t);
 
-    t.target = "CGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTC";
-    t.query  = "AGTCGTCGTCCGTAATCGTCCGTCGTCGTCGA";
+    t.target        = "CGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTC";
+    t.query         = "AGTCGTCGTCCGTAATCGTCCGTCGTCGTCGTA";
     t.edit_distance = 4;
     tests.push_back(t);
 
+    t.target        = "GTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTCGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTCGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTC";
+    t.query         = "GTCGTCGTCCGTCGTCGTCCGTCGTCGTCGAAAACGTCGTCCGTCGTCGTCCGTCGTCGAAAACGTCGTCGTCCGTAGTCGTCCGACGTCGTCGTC";
+    t.edit_distance = 10;
+    tests.push_back(t);
+
+    t.target        = "GTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTCGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTCGTCGTCGTCCGTCGTCGTCCGTCGTCGTCGTC";
+    t.query         = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    t.edit_distance = 96;
+    tests.push_back(t);
     return tests;
 }
 
-class TestMyersEditDistance : public ::testing::TestWithParam<TestCaseData>{
+class TestMyersEditDistance : public ::testing::TestWithParam<TestCaseData>
+{
 };
 
-TEST_P(TestMyersEditDistance,TestCases)
+TEST_P(TestMyersEditDistance, TestCases)
 {
     TestCaseData t = GetParam();
 
     int32_t d = myers_compute_edit_distance(t.target, t.query);
-    ASSERT_EQ(d,t.edit_distance);
+    ASSERT_EQ(d, t.edit_distance);
 }
 
 INSTANTIATE_TEST_SUITE_P(TestMyersAlgorithm, TestMyersEditDistance, ::testing::ValuesIn(create_myers_test_cases()));
