a66b9b553197716ffdf57ae3475a582448774da2
Mike Vella <mvella@nvidia.com>
2019-08-16 22:18:00 +0100

    Redirecting diagnostic output to stderr as per minimap2

--- cudamapper/src/main.cpp
+++ cudamapper/src/main.cpp
@@ -28,19 +28,19 @@ int main(int argc, char *argv[])
     // TODO: pass kmer and window size as parameters
     std::unique_ptr<claragenomics::IndexGenerator> index_generator = claragenomics::IndexGenerator::create_index_generator(std::string(argv[1]), 15, 15);
     CGA_LOG_INFO("Created index generator");
-    //std::cout << "Index generator execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
+    std::cerr << "Index generator execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
 
     start_time = std::chrono::high_resolution_clock::now();
     CGA_LOG_INFO("Creating index");
     std::unique_ptr<claragenomics::Index> index = claragenomics::Index::create_index(*index_generator);
     CGA_LOG_INFO("Created index");
-    //std::cout << "Index execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
+    std::cerr << "Index execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
 
     start_time = std::chrono::high_resolution_clock::now();
     CGA_LOG_INFO("Started matcher");
     claragenomics::Matcher matcher(static_cast<claragenomics::IndexCPU&>(*(index.get())));
     CGA_LOG_INFO("Finished matcher");
-    //std::cout << "Matcher execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
+    std::cerr << "Matcher execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
 
     start_time = std::chrono::high_resolution_clock::now();
     CGA_LOG_INFO("Started overlap detector");
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
     auto overlaps = overlapper.get_overlaps(matcher.anchors(), static_cast<claragenomics::IndexCPU&>(*(index.get())));
 
     CGA_LOG_INFO("Finished overlap detector");
-    //std::cout << "Overlap detection execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
+    std::cerr << "Overlap detection execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << "ms" << std::endl;
 
     overlapper.print_paf(overlaps);
     return  0;
