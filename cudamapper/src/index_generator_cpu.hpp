baa1f3a99eee209af7fcb8b1db9961cad02aa5b8
Mike Vella <mvella@nvidia.com>
2019-08-23 23:29:18 +0100

    Fixing missing docs

--- cudamapper/src/index_generator_cpu.hpp
+++ cudamapper/src/index_generator_cpu.hpp
@@ -43,6 +43,8 @@ namespace claragenomics {
         /// \return mapping of internal read id that goes from 0 to number_of_reads-1 to actual read name from the input
         const std::vector<std::string>& read_id_to_read_name() const override;
 
+        /// \brief returns mapping of internal read id that goes from 0 to number_of_reads-1 to read length
+        /// \return mapping of internal read id that goes from 0 to number_of_reads-1 to read length
         const std::vector<std::uint32_t>& read_id_to_read_length() const override;
 
         /// \brief returns number of reads
