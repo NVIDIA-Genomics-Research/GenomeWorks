d7f1fcd5a95ff5f30b3da8346d4502df5bdbee99
Mike Vella <mvella@nvidia.com>
2019-08-26 00:29:09 +0100

    Added documentation for OverlapperTriggered::fuse_overlaps

--- cudamapper/src/overlapper_triggered.hpp
+++ cudamapper/src/overlapper_triggered.hpp
@@ -34,6 +34,30 @@ namespace claragenomics {
 
         /// \brief given a vector of overlaps, combines all overlaps from the same read pair
         ///
+        /// If two or more overlaps come from the same read pair they are combined into one large overlap:
+        /// Example:
+        /// Overlap 1:
+        ///   Query ID = 18
+        ///   Target ID = 42
+        ///   Query start = 420
+        ///   Query end = 520
+        ///   Target start = 783
+        ///   Target end = 883
+        /// Overlap 2:
+        ///   Query ID = 18
+        ///   Target ID = 42
+        ///   Query start = 900
+        ///   Query end = 1200
+        ///   Target start = 1200
+        ///   Target end = 1500
+        /// Fused overlap:
+        ///   Query ID = 18
+        ///   Target ID = 42
+        ///   Query start = 420
+        ///   Query end = 1200
+        ///   Target start = 783
+        ///   Target end = 1500
+        ///
         /// \param unfused_overlaps vector of overlaps, sorted by (query_id, target_id) combination and query_start_position
         /// \return vector of overlaps
         std::vector<Overlap> fuse_overlaps(std::vector<Overlap> unfused_overlaps);
