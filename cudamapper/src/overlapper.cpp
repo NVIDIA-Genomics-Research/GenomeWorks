c06f3b0a8a230ff3f75e1189d4d99b1774124ed6
Mike Vella <mvella@nvidia.com>
2019-08-26 00:21:03 +0100

    Fix for loop

--- cudamapper/src/overlapper.cpp
+++ cudamapper/src/overlapper.cpp
@@ -15,7 +15,7 @@ namespace claragenomics {
 
     std::vector<Overlap> Overlapper::filter_overlaps(const std::vector<Overlap> &overlaps,size_t  min_residues, size_t  min_overlap_len) {
         std::vector<Overlap> filtered_overlaps;
-        for(auto overlap: overlaps){
+        for(const auto& overlap: overlaps){
             if ((overlap.num_residues_ >= min_residues) &&
                 ((overlap.query_end_position_in_read_ - overlap.query_start_position_in_read_) > min_overlap_len)){
                 filtered_overlaps.push_back(overlap);
