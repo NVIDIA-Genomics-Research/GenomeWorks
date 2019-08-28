aabff4ae1183421db7a19659d9f3dab13bbef199
Joyjit Daw <jdaw@nvidia.com>
2019-08-28 14:08:48 -0400

    [packaging] add basic deb packaging

---
+++ cmake/Packaging.cmake
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+
+SET(CPACK_GENERATOR "DEB")
+SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "NVIDIA Corporation")
+set(CPACK_PACKAGE_VERSION "${CGA_VERSION}")
+
+include(CPack)
