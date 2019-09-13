e019a5d69d021d9b549cc5432b7e2a0d93a466c1
Joyjit Daw <jdaw@nvidia.com>
2019-09-13 12:36:45 -0400

    [pycga] add aligner python API tests

--- pyclaragenomics/test/test_cudaaligner_bindings.py
+++ pyclaragenomics/test/test_cudaaligner_bindings.py
@@ -12,20 +12,90 @@ import pytest
 
 from claragenomics.bindings.cudaaligner import CudaAlignerBatch
 import claragenomics.bindings.cuda as cuda
+from claragenomics.simulators.genomesim import PoissonGenomeSimulator
+from claragenomics.simulators.readsim import NoisyReadSimulator
 
 
 @pytest.mark.gpu
-def test_cudapoa_simple_batch():
+@pytest.mark.parametrize("query, target, cigar", [
+    ("AAAAAAA", "TTTTTTT", "7M"),
+    ("AAATC", "TACGTTTT", "3M1I2M2I"),
+    ("TACGTA", "ACATAC", "1D5M1I"),
+    ("TGCA", "ATACGCT", "1I1M2I3M"),
+    pytest.param("ACGT", "TCGA", "5M", marks=pytest.mark.xfail),
+    ])
+def test_cudaaligner_simple_batch(query, target, cigar):
+    """Test valid calculation of alignments by checking cigar strings.
+    """
     stream = cuda.CudaStream()
-    batch = CudaAlignerBatch(100, 100, 10, "global", stream, 0)
-    batch.add_alignment("AAAAAAA", "TTTTTTT")
-    batch.add_alignment("AAATC", "TACGTTTT")
-    batch.add_alignment("TACGTA", "ACATAC")
-    batch.add_alignment("TGCA", "ATACGCT")
+    batch = CudaAlignerBatch(len(query), len(target), 1, "global", stream, 0)
+    batch.add_alignment(query, target)
     batch.align_all()
     alignments = batch.get_alignments()
 
-    print("")
-    for alignment in alignments:
-        print(alignment.query, alignment.target, alignment.cigar, alignment.alignment_type,
-                alignment.alignment, alignment.format_alignment)
+    assert(len(alignments) == 1)
+    assert(alignments[0].cigar == cigar)
+
+
+@pytest.mark.gpu
+@pytest.mark.parametrize("ref_length, num_alignments", [
+    (5000, 30),
+    (10000, 10),
+    (500, 100)
+    ])
+def test_cudaaligner_long_alignments(ref_length, num_alignments):
+    """Test varying batches of long and short alignments and check for successful
+    completion of alignment.
+    """
+    genome_sim = PoissonGenomeSimulator()
+    read_sim = NoisyReadSimulator()
+
+    batch = CudaAlignerBatch(ref_length, ref_length, num_alignments, "global", None, 0)
+
+    for _ in range(num_alignments):
+        reference = genome_sim.build_reference(ref_length)
+        query = read_sim.generate_read(reference, ref_length, insertion_error_rate=0.0)
+        target = read_sim.generate_read(reference, ref_length, insertion_error_rate=0.0)
+
+        batch.add_alignment(query, target)
+
+    batch.align_all()
+    batch.get_alignments()
+
+
+@pytest.mark.gpu
+@pytest.mark.parametrize("max_seq_len, max_alignments, seq_len, num_alignments, should_succeed", [
+    (1000, 100, 10000, 10, False),
+    (1000, 100, 100, 10, True),
+    (1000, 100, 1000, 100, True),
+    (100, 10, 100, 1000, False),
+    ])
+def test_cudaaligner_various_arguments(max_seq_len, max_alignments, seq_len, num_alignments, should_succeed):
+    """
+    Pass legal and illegal arguments, and test for correct exception throwing behavior.
+    """
+    genome_sim = PoissonGenomeSimulator()
+    read_sim = NoisyReadSimulator()
+
+    batch = CudaAlignerBatch(max_seq_len, max_seq_len, max_alignments, "global", None, 0)
+
+    try:
+        for _ in range(num_alignments):
+            reference = genome_sim.build_reference(seq_len)
+            query = read_sim.generate_read(reference, seq_len, insertion_error_rate=0.0)
+            target = read_sim.generate_read(reference, seq_len, insertion_error_rate=0.0)
+
+            batch.add_alignment(query, target)
+
+        batch.align_all()
+
+        if should_succeed:
+            assert(True)
+            return
+    except RuntimeError:
+        if not should_succeed:
+            assert(True)
+            return
+
+    # If not returned in any of the above scenarios, then fail.
+    assert(False)
