c759c710274319f49b5d716a941138f1e7c1d179
Joyjit Daw <jdaw@nvidia.com>
2019-11-27 19:53:24 -0500

    [docs] update checked in doxygen docs

--- docs/cpp/search/all_c.js
+++ docs/cpp/search/all_c.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['position_5fin_5fread_82',['position_in_read',['../classclaragenomics_1_1cudamapper_1_1SketchElement.html#a0ddea36eed82ea3a9d1fa7ace61051bc',1,'claragenomics::cudamapper::SketchElement']]],
-  ['positions_5fin_5freads_83',['positions_in_reads',['../classclaragenomics_1_1cudamapper_1_1Index.html#a5c75f11d1683e4f863f14a4088a36306',1,'claragenomics::cudamapper::Index']]],
-  ['print_5fpaf_84',['print_paf',['../group__cudamapper.html#ga54fedd97206ad1691f7b9d9966d01371',1,'claragenomics::cudamapper::Overlapper']]]
+  ['pairhash_105',['PairHash',['../structclaragenomics_1_1PairHash.html',1,'claragenomics']]],
+  ['position_5fin_5fread_106',['position_in_read',['../classclaragenomics_1_1cudamapper_1_1SketchElement.html#a0ddea36eed82ea3a9d1fa7ace61051bc',1,'claragenomics::cudamapper::SketchElement']]],
+  ['positions_5fin_5freads_107',['positions_in_reads',['../classclaragenomics_1_1cudamapper_1_1Index.html#a66e5d024befc410221610f454eff7183',1,'claragenomics::cudamapper::Index']]],
+  ['print_5fpaf_108',['print_paf',['../group__cudamapper.html#ga54fedd97206ad1691f7b9d9966d01371',1,'claragenomics::cudamapper::Overlapper']]]
 ];
--- docs/cpp/search/classes_1.js
+++ docs/cpp/search/classes_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['batch_132',['Batch',['../classclaragenomics_1_1cudapoa_1_1Batch.html',1,'claragenomics::cudapoa']]]
+  ['batch_158',['Batch',['../classclaragenomics_1_1cudapoa_1_1Batch.html',1,'claragenomics::cudapoa']]]
 ];
