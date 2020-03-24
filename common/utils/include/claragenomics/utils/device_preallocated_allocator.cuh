98c1a9dea470499894983b9dacc85337a0d89d7f
Andreas Hehn <ahehn@nvidia.com>
2020-03-24 11:32:48 +0100

    [common] preallocated allocator: minor changes:
    * initialize stream argument of first memory block
    * make create_buffer(buffer_size) use argument instead of buffer_size_ member

--- common/utils/include/claragenomics/utils/device_preallocated_allocator.cuh
+++ common/utils/include/claragenomics/utils/device_preallocated_allocator.cuh
@@ -58,7 +58,7 @@ public:
         , buffer_ptr_(create_buffer(buffer_size))
     {
         assert(buffer_size > 0);
-        free_blocks_.push_back({0, buffer_size});
+        free_blocks_.push_back({0, buffer_size, 0});
     }
 
     DevicePreallocatedAllocator(const DevicePreallocatedAllocator&) = delete;
@@ -116,11 +116,11 @@ private:
     /// @brief allocates the underlying buffer
     /// @param buffer_size
     /// @return allocated shared_ptr
-    std::unique_ptr<char, void (*)(char*)> create_buffer(size_t buffer_size)
+    static std::unique_ptr<char, void (*)(char*)> create_buffer(size_t buffer_size)
     {
         // shared_ptr creation packed in a function so it can be used in constructor's initilaization list
         void* ptr = nullptr;
-        CGA_CU_CHECK_ERR(cudaMalloc(&ptr, buffer_size_));
+        CGA_CU_CHECK_ERR(cudaMalloc(&ptr, buffer_size));
         auto ret_val = std::unique_ptr<char, void (*)(char*)>(static_cast<char*>(ptr),
                                                               [](char* ptr) {
                                                                   CGA_CU_ABORT_ON_ERR(cudaFree(ptr));
