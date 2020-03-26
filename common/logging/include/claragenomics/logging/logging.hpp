50a697d7091d9ce125f5efce2093386e20c7166a
Andreas Hehn <ahehn@nvidia.com>
2020-03-26 14:35:42 +0100

    [common] Remove utils dependency from logging library

--- common/logging/include/claragenomics/logging/logging.hpp
+++ common/logging/include/claragenomics/logging/logging.hpp
@@ -36,8 +36,6 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-#include <claragenomics/utils/cudaversions.hpp>
-
 /// \ingroup logging
 /// \{
 
