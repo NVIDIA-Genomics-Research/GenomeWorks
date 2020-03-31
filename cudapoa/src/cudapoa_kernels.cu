b703655c3ffd46281fe4d817f5e04554b67ee0eb
ramin <rmafi@nvidia.com>
2020-03-30 20:16:19 -0400

    changed bool use32bitInt definition, will add a modified version for banded.

--- cudapoa/src/cudapoa_kernels.cu
+++ cudapoa/src/cudapoa_kernels.cu
@@ -710,7 +710,7 @@ void generatePOA(claragenomics::cudapoa::OutputDetails* output_details_d,
                  const BatchSize& batch_size)
 {
     // a decision flag to determine proper type definition for ScoreT
-    const bool use_32_bit_for_ScoreT = use32bitInt(batch_size.max_sequence_size, match_score);
+    const bool use_32_bit_for_ScoreT = use32bitInt(batch_size, match_score, gap_score);
 
     if (use_32_bit_for_ScoreT)
     {
@@ -748,10 +748,12 @@ void generatePOA(claragenomics::cudapoa::OutputDetails* output_details_d,
     }
 }
 
-bool use32bitInt(int32_t max_sequence_size, int32_t match_score)
+bool use32bitInt(const BatchSize& batch_size, const int16_t match_score, const int16_t gap_score)
 {
     // a decision flag to determine proper type definition for ScoreT, factor 2 is selected ad-hoc
-    return (max_sequence_size * match_score * 2 > INT16_MAX);
+    bool hitting_max_limit = batch_size.max_sequence_size * match_score > INT16_MAX;
+    bool hitting_min_limit = (batch_size.max_sequence_size + batch_size.max_matrix_graph_dimension) * (-gap_score) > (INT16_MAX + 1);
+    return (hitting_max_limit || hitting_min_limit);
 }
 
 } // namespace cudapoa
--- cudapoa/src/cudapoa_kernels.cuh
+++ cudapoa/src/cudapoa_kernels.cuh
@@ -297,7 +297,7 @@ void generateConsensusTestHost(uint8_t* nodes,
                                uint16_t* node_alignment_count,
                                uint32_t max_limit_consensus_size);
 
-bool use32bitInt(int32_t max_sequence_size, int32_t match_score);
+bool use32bitInt(const BatchSize& batch_size, const int16_t match_score, const int16_t gap_score);
 
 } // namespace cudapoa
 
