cf251cf2e5fbcbea08fac6b37713b75e36840fc6
Mike Vella <vellamike@gmail.com>
2020-04-02 01:13:37 +0100

    Style changes and imrpoving inline documentation

--- cudamapper/src/overlapper.cpp
+++ cudamapper/src/overlapper.cpp
@@ -231,8 +231,8 @@ void Overlapper::print_paf(const std::vector<Overlap>& overlaps, const std::vect
                     overlap.target_start_position_in_read_,
                     overlap.target_end_position_in_read_,
                     overlap.num_residues_ * k, // Print out the number of residue matches multiplied by kmer size to get approximate number of matching bases
-                    std::max(std::abs(int(overlap.target_start_position_in_read_) - int(overlap.target_end_position_in_read_)),
-                             abs(int(overlap.query_start_position_in_read_) - int(overlap.query_end_position_in_read_))), //Approximate alignment length
+                    std::max(std::abs(static_cast<int>(overlap.target_start_position_in_read_) - static_cast<int>(overlap.target_end_position_in_read_)),
+                             abs(static_cast<int>(overlap.query_start_position_in_read_) - static_cast<int>(overlap.query_end_position_in_read_))), //Approximate alignment length
                     255);
         // If CIGAR string is generated, output in PAF.
         if (cigar.size() != 0)
