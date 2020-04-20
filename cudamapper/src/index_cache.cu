9e9e1386ea594a2865367ec936d7e219c23566c3
Joyjit Daw <jdaw@nvidia.com>
2020-04-19 23:03:38 -0400

    Revert "[cudamapper] add shared lib generation of cudamapper"
    This reverts commit f9157162139baa07d8889a62f20b0bf525906046.

--- cudamapper/src/index_cache.cu
+++ cudamapper/src/index_cache.cu
@@ -10,7 +10,7 @@
 
 #include "index_cache.cuh"
 
-#include "index_host_copy.cuh"
+#include "index_host_copy.cu"
 
 #include <claragenomics/cudamapper/index.hpp>
 #include <claragenomics/io/fasta_parser.hpp>
