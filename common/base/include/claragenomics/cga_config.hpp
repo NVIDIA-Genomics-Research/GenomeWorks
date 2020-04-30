aa6c4ef0de5d272dc2b04e57c0cb7790195d6c25
Andreas Hehn <ahehn@nvidia.com>
2020-04-30 20:15:29 +0200

    [common] Fixed cga_config macros for host-only code

--- common/base/include/claragenomics/cga_config.hpp
+++ common/base/include/claragenomics/cga_config.hpp
@@ -9,6 +9,8 @@
 */
 
 #pragma once
+
+#ifdef __CUDACC_VER_MAJOR__
 /// \def CGA_CUDA_BEFORE_XX_X
 /// \brief Macros to enable/disable CUDA version specific code
 #define CGA_CUDA_BEFORE_XX_X 1
@@ -19,6 +21,7 @@
 
 #if (__CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ < 2)
 #define CGA_CUDA_BEFORE_9_2
+#endif
 #endif
 
 /// \def CGA_CONSTEXPR
