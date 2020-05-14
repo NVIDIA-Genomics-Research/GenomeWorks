60f7bd5c91128a59ccafc5f3bb626f3cd3c63782
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-05-14 14:43:24 -0400

    [common] Add std::string_view and std::experimental::string_view using an alias in claragenomics/types.hpp

--- common/base/include/claragenomics/types.hpp
+++ common/base/include/claragenomics/types.hpp
@@ -14,8 +14,10 @@
 
 #if __cplusplus >= 201703
 #include <optional>
+#include <string_view>
 #else
 #include <experimental/optional>
+#include <experimental/string_view>
 #endif
 
 namespace claragenomics
@@ -40,11 +42,13 @@ template <typename T>
 using cga_optional_t                = std::optional<T>;
 using cga_nullopt_t                 = std::nullopt_t;
 constexpr cga_nullopt_t cga_nullopt = std::nullopt;
+using cga_string_view_t             = std::string_view;
 #else
 template <typename T>
 using cga_optional_t                = std::experimental::optional<T>;
 using cga_nullopt_t                 = std::experimental::nullopt_t;
 constexpr cga_nullopt_t cga_nullopt = std::experimental::nullopt;
+using cga_string_view_t = std::experimental::string_view;
 #endif
 
 } // namespace claragenomics
