0ed28b627a0ad8b0e648b9c770ef7cf48e225b42
Rajesh K Ilango <rilango@nvidia.com>
2020-06-04 14:05:37 -0700

    Re-adjust filters value applied to PAF output
    Re-adjust the value of min residues and min overlap lenght. Also add filter to
    target query.

--- cudamapper/src/main.cu
+++ cudamapper/src/main.cu
@@ -134,8 +134,8 @@ struct ApplicationParameteres
     std::int32_t target_index_size              = 30;    // t
     double filtering_parameter                  = 1.0;   // F
     std::int32_t alignment_engines              = 0;     // a
-    std::int32_t min_residues                   = 10;    // r
-    std::int32_t min_overlap_len                = 500;   // l
+    std::int32_t min_residues                   = 100;   // r
+    std::int32_t min_overlap_len                = 2000;  // l
     std::int32_t min_bases_per_residue          = 100;   // b
     float min_overlap_fraction                  = 0.95;  // z
     bool perform_overlap_end_rescue             = false; // R
