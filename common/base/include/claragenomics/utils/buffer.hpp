9f3cc8437eedce9c453d34854ce4469b67215827
Milos Maric <mmaric@nvidia.com>
2020-06-08 17:40:24 +0200

    [common] Using backslash instead of at in Doxygen comments

--- common/base/include/claragenomics/utils/buffer.hpp
+++ common/base/include/claragenomics/utils/buffer.hpp
@@ -18,10 +18,10 @@ namespace claraparabricks
 namespace genomeworks
 {
 
-/// @brief Container for an array of elements of type `T` in host or device memory.
+/// /brief Container for an array of elements of type `T` in host or device memory.
 ///        `T` must be trivially copyable.
-/// @tparam T The object's type
-/// @tparam Allocator The allocator's type
+/// /tparam T The object's type
+/// /tparam Allocator The allocator's type
 template <typename T, typename Allocator>
 class buffer
 {
@@ -45,11 +45,11 @@ public:
 
     buffer& operator=(const buffer& other) = delete;
 
-    /// @brief This constructor creates \p buffer with the given size.
-    /// @param n The number of elements to create
-    /// @param allocator The allocator to use by this buffer.
-    /// @param stream The CUDA stream to be associated with this allocation. Default is stream 0.
-    /// @tparam AllocatorIn Type of input allocator. If AllocatorIn::value_type is different than T AllocatorIn will be converted to Allocator<T> if possible, compilation will fail otherwise
+    /// /brief This constructor creates \p buffer with the given size.
+    /// /param n The number of elements to create
+    /// /param allocator The allocator to use by this buffer.
+    /// /param stream The CUDA stream to be associated with this allocation. Default is stream 0.
+    /// /tparam AllocatorIn Type of input allocator. If AllocatorIn::value_type is different than T AllocatorIn will be converted to Allocator<T> if possible, compilation will fail otherwise
     template <typename AllocatorIn = Allocator>
     explicit buffer(size_type n           = 0,
                     AllocatorIn allocator = AllocatorIn(),
@@ -67,10 +67,10 @@ public:
         }
     }
 
-    /// @brief This constructor creates an empty \p buffer.
-    /// @param allocator The allocator to use by this buffer.
-    /// @param stream The CUDA stream to be associated with this allocation. Default is stream 0.
-    /// @tparam AllocatorIn Type of input allocator. If AllocatorIn::value_type is different than T AllocatorIn will be converted to Allocator<T> if possible, compilation will fail otherwise
+    /// /brief This constructor creates an empty \p buffer.
+    /// /param allocator The allocator to use by this buffer.
+    /// /param stream The CUDA stream to be associated with this allocation. Default is stream 0.
+    /// /tparam AllocatorIn Type of input allocator. If AllocatorIn::value_type is different than T AllocatorIn will be converted to Allocator<T> if possible, compilation will fail otherwise
     template <typename AllocatorIn, std::enable_if_t<std::is_class<AllocatorIn>::value, int> = 0> // for calls like buffer(5, stream) the other constructor should be used -> only enable if AllocatorIn is a class.
     explicit buffer(AllocatorIn allocator, cudaStream_t stream = 0)
         : buffer(0, allocator, stream)
@@ -78,9 +78,9 @@ public:
         static_assert(std::is_trivially_copyable<value_type>::value, "buffer only supports trivially copyable types and classes, because destructors will not be called.");
     }
 
-    /// @brief Move constructor moves from another \p buffer.
+    /// /brief Move constructor moves from another \p buffer.
     /// Buffer to move from (rhs) is left in an empty state (size = capacity = 0), with the original stream and allocator.
-    /// @param rhs The bufer to move.
+    /// /param rhs The bufer to move.
     buffer(buffer&& rhs)
         : _data(std::exchange(rhs._data, nullptr))
         , _size(std::exchange(rhs._size, 0))
@@ -89,10 +89,10 @@ public:
     {
     }
 
-    /// @brief Move assign operator moves from another \p buffer.
+    /// /brief Move assign operator moves from another \p buffer.
     /// Buffer to move from (rhs) is left in an empty state (size = capacity = 0), with the original stream and allocator.
-    /// @param rhs The buffer to move.
-    /// @return refrence to this buffer.
+    /// /param rhs The buffer to move.
+    /// /return refrence to this buffer.
     buffer& operator=(buffer&& rhs)
     {
         _data      = std::exchange(rhs._data, nullptr);
@@ -102,7 +102,7 @@ public:
         return *this;
     }
 
-    /// @brief This destructor releases the buffer allocation, returning it to the allocator.
+    /// /brief This destructor releases the buffer allocation, returning it to the allocator.
     ~buffer()
     {
         if (nullptr != _data)
@@ -111,40 +111,40 @@ public:
         }
     }
 
-    /// @brief This method returns a pointer to this buffer's first element.
-    /// @return A pointer to the first element of this buffer.
+    /// /brief This method returns a pointer to this buffer's first element.
+    /// /return A pointer to the first element of this buffer.
     value_type* data() { return _data; }
 
-    /// @brief This method returns a const_pointer to this buffer's first element.
-    /// @return A const pointer to the first element of this buffer.
+    /// /brief This method returns a const_pointer to this buffer's first element.
+    /// /return A const pointer to the first element of this buffer.
     const value_type* data() const { return _data; }
 
-    /// @brief This method returns the number of elements in this buffer.
+    /// /brief This method returns the number of elements in this buffer.
     size_type size() const { return _size; }
 
-    /// @brief This method returns an iterator pointing to the beginning of this buffer.
+    /// /brief This method returns an iterator pointing to the beginning of this buffer.
     iterator begin() { return _data; }
 
-    /// @brief This method returns an const_iterator pointing to the beginning of this buffer.
+    /// /brief This method returns an const_iterator pointing to the beginning of this buffer.
     const_iterator begin() const { return _data; }
 
-    /// @brief This method returns a iterator pointing to one element past the last of this buffer.
-    /// @return begin() + size().
+    /// /brief This method returns a iterator pointing to one element past the last of this buffer.
+    /// /return begin() + size().
     iterator end() { return _data + _size; }
 
-    /// @brief This method returns a const_iterator pointing to one element past the last of this buffer.
-    /// @return begin() + size().
+    /// /brief This method returns a const_iterator pointing to one element past the last of this buffer.
+    /// /return begin() + size().
     const_iterator end() const { return _data + _size; }
 
-    /// @brief This method returns the associated stream.
-    /// @return associated stream.
+    /// /brief This method returns the associated stream.
+    /// /return associated stream.
     cudaStream_t get_stream() const { return _stream; }
 
-    /// @brief This method returns the allocator used to allocate memory for this buffer.
-    /// @return allocator.
+    /// /brief This method returns the allocator used to allocate memory for this buffer.
+    /// /return allocator.
     allocator_type get_allocator() const { return _allocator; }
 
-    /// @brief This method releases the memory and resizes the buffer to 0.
+    /// /brief This method releases the memory and resizes the buffer to 0.
     void clear_and_free()
     {
         if (_size > 0)
@@ -155,11 +155,11 @@ public:
         }
     }
 
-    /// @brief This method resizes buffer and discards old data
+    /// /brief This method resizes buffer and discards old data
     ///
     /// If old data is still needed it is recommended to crete a new buffer and manually copy the data from the old buffer
     ///
-    /// @param new_size Number of elements
+    /// /param new_size Number of elements
     void clear_and_resize(const size_type new_size)
     {
         assert(new_size >= 0);
@@ -174,9 +174,9 @@ public:
         _data = _size > 0 ? _allocator.allocate(_size, _stream) : nullptr;
     }
 
-    /// @brief This method swaps the contents of two buffers.
-    /// @param a One buffer.
-    /// @param b The other buffer.
+    /// /brief This method swaps the contents of two buffers.
+    /// /param a One buffer.
+    /// /param b The other buffer.
     friend void swap(buffer& a, buffer& b) noexcept
     {
         using std::swap;
