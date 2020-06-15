3cfa0fa40231a0de42a1804c60b7a32182db0576
Joyjit Daw <jdaw@nvidia.com>
2020-06-14 20:50:52 -0400

    [cudapoa] remove cxx optim flags for cudapoa only

--- cmake/CXX.cmake
+++ cmake/CXX.cmake
@@ -15,11 +15,3 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fopenmp")
 # Add -O2 optimization to debug builds to speed up runtime.
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O2")
-
-if(NOT DEFINED cga_cuda_before_10_0)
-    message(FATAL_ERROR "variable cga_cuda_before_10 is not defined yet. Please make sure CUDA.cmake is loaded first.")
-elseif(cga_cuda_before_10_0)
-    message(STATUS "Remove -O optimization when building for CUDA < 10 as it causes compilation issues.")
-    string(REGEX REPLACE "-O[0-3]" "" CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_RELEASE})
-    string(REGEX REPLACE "-O[0-3]" "" CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG})
-endif()
