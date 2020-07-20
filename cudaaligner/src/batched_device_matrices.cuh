697036f9c38d4f58499fdee0a3cbfadb9ae1280a
Andreas Hehn <ahehn@nvidia.com>
2020-07-20 14:09:45 +0200

    [cudaaligner] added append_matrix() to batched_device_matrices

--- cudaaligner/src/batched_device_matrices.cuh
+++ cudaaligner/src/batched_device_matrices.cuh
@@ -20,7 +20,10 @@
 
 #include <claraparabricks/genomeworks/utils/cudautils.hpp>
 #include <claraparabricks/genomeworks/utils/limits.cuh>
+#include <claraparabricks/genomeworks/utils/signed_integer_utils.hpp>
 #include <claraparabricks/genomeworks/utils/device_buffer.hpp>
+
+#include <thrust/system/cuda/experimental/pinned_allocator.h>
 
 #include <tuple>
 #include <cassert>
@@ -77,9 +80,9 @@ public:
     }
 
 private:
-    T* data_;
-    int32_t n_rows_;
-    int32_t n_cols_;
+    T* data_        = nullptr;
+    int32_t n_rows_ = 0;
+    int32_t n_cols_ = 0;
 };
 
 template <typename T>
@@ -135,34 +138,82 @@ public:
         friend class batched_device_matrices<T>;
     };
 
+    batched_device_matrices(int64_t max_elements, DefaultDeviceAllocator allocator, cudaStream_t stream)
+        : storage_(max_elements, allocator, stream)
+        , offsets_(allocator, stream)
+        , dev_(1, allocator, stream)
+        , offsets_host_(1, 0)
+    {
+    }
+
     batched_device_matrices(int32_t n_matrices, int32_t max_elements_per_matrix, DefaultDeviceAllocator allocator, cudaStream_t stream)
         : storage_(static_cast<size_t>(n_matrices) * static_cast<size_t>(max_elements_per_matrix), allocator, stream)
         , offsets_(n_matrices + 1, allocator, stream)
-        , n_matrices_(n_matrices)
+        , dev_(1, allocator, stream)
+        , offsets_host_(n_matrices + 1)
     {
         assert(n_matrices >= 0);
         assert(max_elements_per_matrix >= 0);
-        GW_CU_CHECK_ERR(cudaMalloc(reinterpret_cast<void**>(&dev_), sizeof(device_interface)));
         GW_CU_CHECK_ERR(cudaMemsetAsync(storage_.data(), 0, storage_.size() * sizeof(T), stream));
-        std::vector<ptrdiff_t> offsets(n_matrices + 1);
+
         for (int32_t i = 0; i < n_matrices + 1; ++i)
         {
-            offsets[i] = max_elements_per_matrix * i;
+            offsets_host_[i] = max_elements_per_matrix * i;
         }
-        cudautils::device_copy_n(offsets.data(), n_matrices + 1, offsets_.data(), stream);
-        device_interface tmp(storage_.data(), offsets_.data(), n_matrices_);
-        GW_CU_CHECK_ERR(cudaMemcpyAsync(dev_, &tmp, sizeof(device_interface), cudaMemcpyHostToDevice, stream));
-        GW_CU_CHECK_ERR(cudaStreamSynchronize(stream)); // sync because local vars will be destroyed.
+
+        construct_device_matrices_async(stream);
     }
 
-    ~batched_device_matrices()
-    {
-        GW_CU_ABORT_ON_ERR(cudaFree(dev_));
-    }
+    ~batched_device_matrices() = default;
+
+    batched_device_matrices(batched_device_matrices const&) = delete;
+    batched_device_matrices& operator=(batched_device_matrices const&) = delete;
+    batched_device_matrices(batched_device_matrices&&)                 = default;
+    batched_device_matrices& operator=(batched_device_matrices&&) = default;
 
     device_interface* get_device_interface()
     {
-        return dev_;
+        return dev_.data();
+    }
+
+    bool append_matrix(int32_t max_elements)
+    {
+        assert(offsets_host_.size() >= 1);
+        const ptrdiff_t begin = offsets_host_.back();
+        if (begin + max_elements > get_size(storage_))
+        {
+            return false;
+        }
+        offsets_host_.push_back(begin + max_elements);
+        return true;
+    }
+
+    int32_t number_of_matrices() const
+    {
+        assert(offsets_host_.size() >= 1);
+        return get_size<int32_t>(offsets_host_) - 1;
+    }
+
+    int64_t remaining_free_matrix_elements() const
+    {
+        assert(offsets_host_.size() >= 1);
+        return get_size<int64_t>(storage_) - offsets_host_.back();
+    }
+
+    void clear()
+    {
+        offsets_host_.clear();
+        offsets_host_.push_back(0);
+    }
+
+    void construct_device_matrices_async(cudaStream_t stream)
+    {
+        assert(offsets_host_.size() >= 1);
+        offsets_.clear_and_resize(offsets_host_.size());
+        dev_interface_host_.clear();
+        dev_interface_host_.emplace_back(storage_.data(), offsets_.data(), get_size<int32_t>(offsets_host_) - 1);
+        cudautils::device_copy_n(offsets_host_.data(), offsets_host_.size(), offsets_.data(), stream);
+        cudautils::device_copy_n(dev_interface_host_.data(), 1, dev_.data(), stream);
     }
 
     matrix<T> get_matrix(int32_t id, int32_t n_rows, int32_t n_cols, cudaStream_t stream)
@@ -171,25 +222,26 @@ public:
         assert(n_rows >= 0);
         assert(n_cols >= 0);
 
-        if (id >= n_matrices_)
+        if (id >= get_size(offsets_) - 1)
             throw std::runtime_error("Requested id is out of bounds.");
 
-        std::array<ptrdiff_t, 2> offsets;
-        cudautils::device_copy_n(offsets_.data() + id, 2, offsets.data(), stream);
         matrix<T> m(n_rows, n_cols);
-        GW_CU_CHECK_ERR(cudaStreamSynchronize(stream));
-        if (n_rows * n_cols > offsets[1] - offsets[0])
+        if (n_rows * n_cols > offsets_host_[id + 1] - offsets_host_[id])
             throw std::runtime_error("Requested matrix size is larger than allocated memory on device.");
-        cudautils::device_copy_n(storage_.data() + offsets[0], n_rows * n_cols, m.data(), stream);
+        cudautils::device_copy_n(storage_.data() + offsets_host_[id], n_rows * n_cols, m.data(), stream);
         GW_CU_CHECK_ERR(cudaStreamSynchronize(stream));
         return m;
     }
 
 private:
+    template <typename U>
+    using pinned_host_vector = std::vector<U, thrust::system::cuda::experimental::pinned_allocator<U>>;
+
     device_buffer<T> storage_;
     device_buffer<ptrdiff_t> offsets_;
-    device_interface* dev_ = nullptr;
-    int32_t n_matrices_;
+    device_buffer<device_interface> dev_;
+    pinned_host_vector<ptrdiff_t> offsets_host_;
+    pinned_host_vector<device_interface> dev_interface_host_; // just to provide truly async copies
 };
 
 } // namespace cudaaligner
