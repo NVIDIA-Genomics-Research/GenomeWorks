9c4fe45f288116c6e45ee0c2d0bd59f3f1eeb544
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-08-14 01:01:51 -0400

    [cudamapper] Disable CPU fusion by placing an early return in postprocess_overlaps.

--- cudamapper/src/overlapper.cpp
+++ cudamapper/src/overlapper.cpp
@@ -143,6 +143,7 @@ void Overlapper::post_process_overlaps(std::vector<Overlap>& overlaps, const boo
     int num_residues = 0;
     Overlap prev_overlap;
     std::vector<bool> drop_overlap_mask;
+    return;
     if (drop_fused_overlaps)
     {
         drop_overlap_mask.resize(overlaps.size());
