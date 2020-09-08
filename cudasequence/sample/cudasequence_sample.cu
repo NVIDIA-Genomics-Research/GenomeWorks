39036f5d804e91997501615355517e7b1ef37868
Andreas Hehn <ahehn@nvidia.com>
2020-09-08 12:24:21 +0200

    Improved the encoded sequence API
    most important changes:
    * Renamed Collection classes to Vector
    * Introduced SequenceView class
    * Removed Sequence class

--- cudasequence/sample/cudasequence_sample.cu
+++ cudasequence/sample/cudasequence_sample.cu
@@ -17,25 +17,35 @@
 
 // THIS IS AN EARLY DRAFT - WORK IN PROGRESS
 
-__global__ some_kernel(DeviceSequenceCollection::device_interface* p)
-{
-    DeviceSequenceCollection seq = p->get_view();
-    const char internal_representation = seq[i];
-    
-    // QUESTION: Do we need to modify sequences on the device?
-}
 
+// Problems we want to solve:
+// a) Have a representation which can be directly used as matrix index
+// b) (optional) reduce size of representations
 
-__global__ kernel_a(ScoringMatrix::device_interface* m_ptr, DeviceSequence::device_interface* query_ptr, DeviceSequence::device_interface* target_ptr)
+__global__ some_kernel(ScoringMatrix::device_interface* m_ptr, DeviceSequenceVector::device_interface* p)
 {
+    int idx = ...;
     ScoringMatrix::device_interface&  score_matrix = *m_ptr;
-    DeviceSequence::device_interface& query        = *query_ptr;
-    DeviceSequence::device_interface& target       = *target_ptr;
+    SequenceView query  = p->get_seq(idx);
+    SequenceView target = p->get_seq(idx + 1);
 
-    const int32_t score = score_matrix(query[i - 1], target[j - 1]);
+    int size = min(get_size(query), get_size(target));
+    for(int i = 0; i < size; ++i)
+    {
+        int32_t score = score_matrix(query[i], target[j]);
+        // or:
+        ScoringMatrix::value_type score = score_matrix(query[i], target[j]);
+
+        bool is_same = (query[i] == target[i]);
+        char internal_representation = query[i];
+    }
+
+    // QUESTION: Do we need to modify sequences on the device?
+    // -> no, but we need to be able to create sequences.
 }
 
-// alternative:
+
+// FIXME:
 __global__ kernel_a(ScoringMatrix::device_interface* m_ptr, const char* query_encoded, const char* target_encoded);
 
 
@@ -48,43 +58,34 @@ int main()
 
 // Alphabet
 std::shared_ptr<Alphabet> a = make_alphabet("acgt");
+const char internal_rep = a.get_internal_representation('c'); // in case you need to compare against a special base/character.
 
 // Scoring matrix
 ScoringMatrix m(a, m);
 m('a','a') = score;
 m('a','c') = score;
 
-// Sequence - always 5'->3' (or other norm)
-Sequence seq1 = make_sequence(a, "acgtgtaccta", is_reverse_complement); // converts acgt -> [0,1,2,3]
-Sequence seq2 = make_sequence(a, reverse_complement("acgtgtaccta"));
-
-std::string s = seq1.get_forward_string(); // returns std::string acgt
-bool reverse  = seq1.was_reverse_complement();
-
-// Device Sequence 
-DeviceSequence seq_d(seq, stream);
-
-char* str_on_device = ...;
-
-DeviceSequence seq_d2(a, str_on_device, size, is_reverse_complement);
-
+// Sequence vector - Idea 1
 // Either: hidden data format, only accessible via [] operator: +may allow for tighter packing, eg. 4 bases per char.
 // or just a char array with values \in [0, alphabet_size)
+SequenceVector seqs(alphabet, max_elements_total);
+seqs.push_back_string("acgtgtacc", is_reverse_complement);
+bool appended1 = seqs.push_back(seq1);
+bool appended2 = seqs.push_back(seq2);
 
 
-// Sequence vector - Idea 1
-SequenceCollection seqs(max_collection_size);
-seqs.push_back(seq1);
-seqs.push_back(seq2);
+SequenceView seq0 = seqs[0];
+SequenceView seq1(forward_str, size);
+std::string s = seq0.form_forward_string(); // returns std::string acgt
+bool reverse  = seq0.was_reverse_complement();
 
-
-// Device Sequence Collection
-DeviceSequenceCollection seqs_d(seqs, stream);
+// Device Sequence Vector
+DeviceSequenceVector seqs_d(seqs, stream);
 
 const char** device_strings = ...;
 const int64_t** string_sizes = ...;
 int32_t number_of_strings = ...;
-DeviceSequenceCollection seqs_d2(number_of_strings, device_strings, string_sizes);
+DeviceSequenceVector seqs_d2(alphabet, number_of_strings, device_strings, string_sizes);
 
 some_kernel<<<1,1>>>(seqs_d.get_device_interface());
 
