e56c9c8b053ecf83d76db57d11c7ce58ef84f8dd
ramin <rmafi@nvidia.com>
2020-09-25 17:13:41 -0400

    [cudapoa] started unifying NW banded and NW banded adaptive, WIP

--- cudapoa/src/cudapoa_nw_adaptive_banded.cuh
+++ cudapoa/src/cudapoa_nw_adaptive_banded.cuh
@@ -165,7 +165,8 @@ __device__ __forceinline__ void initialize_band_adaptive(ScoreT* scores, int32_t
 template <typename SeqT,
           typename ScoreT,
           typename SizeT,
-          typename TraceT>
+          typename TraceT,
+          bool ADAPTIVE = false>
 __device__ __forceinline__
     int32_t
     runNeedlemanWunschAdaptiveBanded(SeqT* nodes,
@@ -203,48 +204,55 @@ __device__ __forceinline__
     //---------------------------------------------------------
     int32_t band_width = static_band_width;
 
-    if (gradient > 1.1) // ad-hoc rule 1.a
+    if (ADAPTIVE)
     {
-        //                                                                            ad-hoc rule 1.b
-        band_width = max(band_width, cudautils::align<int32_t, CUDAPOA_MIN_BAND_WIDTH>(max_column * 0.08 * gradient));
-    }
-    if (gradient < 0.8) // ad-hoc rule 2.a
-    {
-        //                                                                            ad-hoc rule 2.b
-        band_width = max(band_width, cudautils::align<int32_t, CUDAPOA_MIN_BAND_WIDTH>(max_column * 0.1 / gradient));
+        if (gradient > 1.1) // ad-hoc rule 1.a
+        {
+            //                                                                            ad-hoc rule 1.b
+            band_width = max(band_width, cudautils::align<int32_t, CUDAPOA_MIN_BAND_WIDTH>(max_column * 0.08 * gradient));
+        }
+        if (gradient < 0.8) // ad-hoc rule 2.a
+        {
+            //                                                                            ad-hoc rule 2.b
+            band_width = max(band_width, cudautils::align<int32_t, CUDAPOA_MIN_BAND_WIDTH>(max_column * 0.1 / gradient));
+        }
+
+        // limit band-width for very large reads, ad-hoc rule 3
+        ///ToDo add a check for cases where static_band_width > CUDAPOA_MAX_ADAPTIVE_BAND_WIDTH
+        band_width = min(band_width, CUDAPOA_MAX_ADAPTIVE_BAND_WIDTH);
     }
 
-    // limit band-width for very large reads, ad-hoc rule 3
-    ///ToDo add a check for cases where static_band_width > CUDAPOA_MAX_ADAPTIVE_BAND_WIDTH
-    band_width = min(band_width, CUDAPOA_MAX_ADAPTIVE_BAND_WIDTH);
     // band_shift defines distance of band_start from the scores matrix diagonal, ad-hoc rule 4
     int32_t band_shift = band_width / 2;
-    // rerun code is defined in backtracking loop from previous alignment try
-    // -3 means traceback path was too close to the left bound of band
-    // -4 means traceback path was too close to the right bound of band
-    // Therefore we rerun alignment of the same read, but this time with double band-width and band_shift
-    // further to the left for rerun == -3, and further to the right for rerun == -4.
-    if (rerun == -3)
+
+    if(ADAPTIVE)
     {
-        // ad-hoc rule 5
-        band_width *= 2;
-        band_shift *= 2.5;
-    }
-    if (rerun == -4)
-    {
-        // ad-hoc rule 6
-        band_width *= 2;
-        band_shift *= 1.5;
+        // rerun code is defined in backtracking loop from previous alignment try
+        // -3 means traceback path was too close to the left bound of band
+        // -4 means traceback path was too close to the right bound of band
+        // Therefore we rerun alignment of the same read, but this time with double band-width and band_shift
+        // further to the left for rerun == -3, and further to the right for rerun == -4.
+        if (rerun == -3)
+        {
+            // ad-hoc rule 5
+            band_width *= 2;
+            band_shift *= 2.5;
+        }
+        if (rerun == -4)
+        {
+            // ad-hoc rule 6
+            band_width *= 2;
+            band_shift *= 1.5;
+        }
+        // check required memory and return error if exceeding max_buffer_size
+        // using float to avoid 64-bit
+        float required_buffer_size = static_cast<float>(graph_count) * static_cast<float>(band_width + CUDAPOA_BANDED_MATRIX_RIGHT_PADDING);
+        if (required_buffer_size > max_buffer_size)
+        {
+            return -2;
+        }
     }
     //---------------------------------------------------------
-
-    // check required memory and return error if exceeding max_buffer_size
-    // using float to avoid 64-bit
-    float required_buffer_size = static_cast<float>(graph_count) * static_cast<float>(band_width + CUDAPOA_BANDED_MATRIX_RIGHT_PADDING);
-    if (required_buffer_size > max_buffer_size)
-    {
-        return -2;
-    }
 
     // Initialise the horizontal boundary of the score matrix, initialising of the vertical boundary is done within the main for loop
     for (int32_t j = lane_idx; j < band_width + CUDAPOA_BANDED_MATRIX_RIGHT_PADDING; j += WARP_SIZE)
@@ -421,23 +429,26 @@ __device__ __forceinline__
             // Check if move is diagonal.
             if (i != 0 && j != 0)
             {
-                if (rerun == 0)
+                if(ADAPTIVE)
                 {
-                    // check if traceback gets too close or hits the band limits, if so stop and rerun with extended band-width
-                    // threshold for proximity to band limits works better if defined proportionate to the sequence length
-                    int32_t threshold = max(1, max_column / 1024); // ad-hoc rule 7
-                    if (j > threshold && j < max_column - threshold)
+                    if (rerun == 0)
                     {
-                        int32_t band_start = get_band_start_for_row_adaptive(i, gradient, band_width, band_shift, max_column);
-                        if (j <= band_start + threshold) // ad-hoc rule 8-a, too close to left bound
+                        // check if traceback gets too close or hits the band limits, if so stop and rerun with extended band-width
+                        // threshold for proximity to band limits works better if defined proportionate to the sequence length
+                        int32_t threshold = max(1, max_column / 1024); // ad-hoc rule 7
+                        if (j > threshold && j < max_column - threshold)
                         {
-                            aligned_nodes = -3;
-                            break;
-                        }
-                        if (j >= (band_start + band_width - threshold)) // ad-hoc rule 8-b, too close to right bound
-                        {
-                            aligned_nodes = -4;
-                            break;
+                            int32_t band_start = get_band_start_for_row_adaptive(i, gradient, band_width, band_shift, max_column);
+                            if (j <= band_start + threshold) // ad-hoc rule 8-a, too close to left bound
+                            {
+                                aligned_nodes = -3;
+                                break;
+                            }
+                            if (j >= (band_start + band_width - threshold)) // ad-hoc rule 8-b, too close to right bound
+                            {
+                                aligned_nodes = -4;
+                                break;
+                            }
                         }
                     }
                 }
