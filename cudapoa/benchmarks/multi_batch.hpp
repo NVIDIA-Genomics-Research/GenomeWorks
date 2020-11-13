3a63d406da80734ae1d9898ca9844710ffd76486
Ashutosh Tadkase <ashutosh.tadkase@gmail.com>
2020-11-12 16:05:11 -0800

    [cudapoa] make format

--- cudapoa/benchmarks/multi_batch.hpp
+++ cudapoa/benchmarks/multi_batch.hpp
@@ -51,8 +51,8 @@ public:
         size_t total = 0, free = 0;
         cudaSetDevice(0);
         cudaMemGetInfo(&free, &total);
-        DefaultDeviceAllocator allocator = create_default_device_allocator(0.901*free);
-        int64_t mem_per_batch = 0.9 * free / num_batches_;
+        DefaultDeviceAllocator allocator = create_default_device_allocator(0.901 * free);
+        int64_t mem_per_batch            = 0.9 * free / num_batches_;
         for (int32_t batch = 0; batch < num_batches_; batch++)
         {
             streams_.emplace_back(make_cuda_stream());
