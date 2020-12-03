9f5ed3d305e31d7b1123a355329b8bd3d62a14bd
Joyjit Daw <jdaw@nvidia.com>
2020-12-03 09:35:35 -0500

    [cudapoa] throw error for unknown error type

--- cudapoa/src/cudapoa.cpp
+++ cudapoa/src/cudapoa.cpp
@@ -39,49 +39,47 @@ void decode_error(StatusType error_type, std::string& error_message, std::string
     switch (error_type)
     {
     case StatusType::exceeded_maximum_poas:
-        error_message = "Kernel Error: Number of groups per batch exceeded maximum POAs";
-        error_hint    = "Suggestion  : Evaluate maximum number of groups per batch using BatchBlock::estimate_max_poas()";
+        error_message = "Kernel Error: Number of groups per batch exceeded maximum POAs.";
+        error_hint    = "Suggestion  : Evaluate maximum number of groups per batch using BatchBlock::estimate_max_poas().";
         break;
     case StatusType::exceeded_maximum_sequence_size:
-        error_message = "Kernel Error: Input read length or output consensus/MSA sequence length exceeded max sequence size";
-        error_hint    = "Suggestion  : Check BatchConfig::max_sequence_size and BatchConfig::max_consensus_size, increase if necessary";
+        error_message = "Kernel Error: Input read length or output consensus/MSA sequence length exceeded max sequence size.";
+        error_hint    = "Suggestion  : Check BatchConfig::max_sequence_size and BatchConfig::max_consensus_size, increase if necessary.";
         break;
     case StatusType::exceeded_maximum_sequences_per_poa:
-        error_message = "Kernel Error: Exceeded maximum number of reads per POA";
-        error_hint    = "Suggestion  : Check BatchConfig::max_sequences_per_poa and increase if necessary";
+        error_message = "Kernel Error: Exceeded maximum number of reads per POA.";
+        error_hint    = "Suggestion  : Check BatchConfig::max_sequences_per_poa and increase if necessary.";
         break;
     case StatusType::node_count_exceeded_maximum_graph_size:
-        error_message = "Kernel Error: Node count exceeded maximum nodes per POA graph";
-        error_hint    = "Suggestion  : Check BatchConfig::max_nodes_per_graph and increase if necessary";
+        error_message = "Kernel Error: Node count exceeded maximum nodes per POA graph.";
+        error_hint    = "Suggestion  : Check BatchConfig::max_nodes_per_graph and increase if necessary.";
         break;
     case StatusType::edge_count_exceeded_maximum_graph_size:
-        error_message = "Kernel Error: Edge count exceeded maximum edges per graph";
-        error_hint    = "Suggestion  : Check default value of CUDAPOA_MAX_NODE_EDGES, note that increasing this macro would increase memory usage per POA";
+        error_message = "Kernel Error: Edge count exceeded maximum edges per graph.";
+        error_hint    = "Suggestion  : Check default value of CUDAPOA_MAX_NODE_EDGES, note that increasing this macro would increase memory usage per POA.";
         break;
     case StatusType::exceeded_adaptive_banded_matrix_size:
-        error_message = "Kernel Error: Allocated buffer for score/traceback matrix in adaptive banding is not large enough";
-        error_hint    = "Suggestion  : Check BatchConfig::matrix_sequence_dimension and increase if necessary";
+        error_message = "Kernel Error: Allocated buffer for score/traceback matrix in adaptive banding is not large enough.";
+        error_hint    = "Suggestion  : Check BatchConfig::matrix_sequence_dimension and increase if necessary.";
         break;
     case StatusType::exceeded_maximum_predecessor_distance:
-        error_message = "Kernel Error: Set value for maximum predecessor distance in Needleman-Wunsch algorithm with traceback buffer is not large enough";
-        error_hint    = "Suggestion  : Check BatchConfig::max_banded_pred_distance and increase if necessary";
+        error_message = "Kernel Error: Set value for maximum predecessor distance in Needleman-Wunsch algorithm with traceback buffer is not large enough.";
+        error_hint    = "Suggestion  : Check BatchConfig::max_banded_pred_distance and increase if necessary.";
         break;
     case StatusType::loop_count_exceeded_upper_bound:
-        error_message = "Kernel Error: Traceback in Needleman-Wunsch algorithm failed";
-        error_hint    = "Suggestion  : You may retry with a different banding mode";
+        error_message = "Kernel Error: Traceback in Needleman-Wunsch algorithm failed.";
+        error_hint    = "Suggestion  : You may retry with a different banding mode.";
         break;
     case StatusType::output_type_unavailable:
-        error_message = "Kernel Error: Output type not available";
-        error_hint    = "Suggestion  : Check MSA/Consensus selection for output type";
+        error_message = "Kernel Error: Output type not available.";
+        error_hint    = "Suggestion  : Check MSA/Consensus selection for output type.";
         break;
     case StatusType::generic_error:
-        error_message = "Unknown error";
+        error_message = "Unknown error.";
         error_hint    = "";
         break;
     default:
-        error_message = "Unknown error";
-        error_hint    = "";
-        break;
+        throw std::runtime_error("Unknown error type detected.");
     }
 }
 
